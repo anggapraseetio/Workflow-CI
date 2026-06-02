4ef81fb2904b060d5a258afa313fc3f18c8858bf
Angga Prasetio <prasetioangga127@gmail.com>
2026-06-02 14:53:30 +0700

    Fix workflow + Docker

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -34,7 +34,8 @@ except Exception as e:
     sys.exit(1)
 
 # ================== MLFLOW TRAINING ==================
-
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("CI-Training")
 with mlflow.start_run(run_name="CI_RandomForest_Tuning"):
     
     param_grid = {
